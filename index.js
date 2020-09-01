f8c0f3943a5c0a3a07a6e473ccbb8bcf72c513d6
night <savagedeveloper792@gmail.com>
2020-09-02 00:36:56 +0530

    Added Ip Check

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
-
+// app.set('trust proxy', true)
 app.get('/',(req,res) => {
+    // console.log(req.connection.remoteAddress)
+    // console.log(req.headers['x-forwarded-for'])
+    console.log(req.ip)
     res.send('Recieved')
+})
+
+app.get('/a',(req,res) => {
+    // console.log(req.connection.remoteAddress)
+    // console.log(req.headers['x-forwarded-for'])
+    var ip = req.headers['x-real-ip'] ;
+    console.log(ip);
+    console.log(Object.keys(req.headers))
+    res.send({status:200})
 })
 
 app.listen(5000)
